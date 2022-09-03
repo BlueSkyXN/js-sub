fcdc4999c4c0fd1d9f69d494f22a7c02b7b3b2ae
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2022-09-03 17:09:19 +0800

    1.5

--- foot.js
+++ foot.js
@@ -10,6 +10,9 @@ document.write("<br>");
 document.write("<a href=\"https://go.000714.xyz/buyvm\" style=\"color: #81e6dc\" align=\"center\">【广告】美国高性价比+大硬盘+大带宽无限流量VPS|</a>"); 
 document.write("<a href=\"https://www.blueskyxn.com/202103/4351.html\" style=\"color: #81e6dc\" align=\"center\">【广告】DNS解锁国际流媒体|</a>"); 
 document.write("<br>");
+document.write("<a href=\"https://www.blueskyxn.com/202209/6513.html\" style=\"color: #81e6dc\" align=\"center\">【广告】Pyproxy 住宅IP代理【中国大陆不可用，不具有绕过防火墙和跨境网关的功能】|</a>"); 
+document.write("<a href=\"https://go.000714.xyz/fbi\" style=\"color: #81e6dc\" align=\"center\">【广告】加密货币与法币的代充代付回收的商家</a>"); 
+document.write("<br>");
 document.write("<a href=\"https://www.blueskyxn.com/tag/wise\" style=\"color: #81e6dc\" align=\"center\">【推荐】WISE：护照直接开户的全球银行账户和借记卡|</a>"); 
 document.write("<a href=\"https://www.patreon.com/BlueSkyXN\" style=\"color: #81e6dc\" align=\"center\">【推荐】更多金融会员制资讯</a>"); 
 /*document.write("<br>");*/
